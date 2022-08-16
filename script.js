62794f41c0633fe163b4297a8e2629e76323eb36
butlerk <38035824+butlerk@users.noreply.github.com>
2022-08-16 15:32:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+//Custom Function
+function calculateTip(){
+    //store the data of inputs
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numPeople = document.getElementById("totalPeople").value;
+
+    //quick validation - check not $0!
+    if(billAmount === "" || serviceQuality == 0) {
+        window.alert("Please enter some values to get this baby up and running!");
+        return; // This will prevent the functions from continuing
+    }
+
+    //Check to see if this input is empty or less than equal to 1
+    if(numPeople === "" || numPeople <= 1) {
+        numPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else{
+        document.getElementById("each").style.display = "block";
+    }
+    
+    //Do some maths!
+    var total = (billAmount * serviceQuality) / numPeople;
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    
+
+    //Display the tip!
+    document.getElementById("totalTip").style.display = "block"
+    document.getElementById("tip").innerHTML = total;
+}
+
+// Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Clicking the button calls our custom function
+document.getElementById("calculate").onclick = function(){calculateTip();};
+
+
+
 
